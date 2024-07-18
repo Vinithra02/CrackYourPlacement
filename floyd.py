bbc77801d37ffeb2ddaead353c78604e5df511af
Vinithra Balaji <141245778+Vinithra02@users.noreply.github.com>
2024-07-18 11:04:45 +0530

    Update floyd.py

--- floyd.py
+++ floyd.py
@@ -13,6 +13,14 @@ class Solution:
             slow=nums[slow]
             fast=nums[fast]
         return slow
-                    
+
+## Here we use the Floyd method mainly used to detect loops in linked list.
+## So to do that, we first consider each value in nums as a pointer to an index. 
+## then like in Floyd, we use the fast and slow pointer to tranverse the array
+## the slow pointer moves one step at a time while the fast pointer moves two steps at a time.
+## when the slow and the fast pointer meet the first time, that is not the solution
+## after we find the first intersection point, we start the slow pointer again from the begginging and then move both slow and fast pointer one step at a time
+## this is because, according to floyd, after the first point of intersection, the distance from the internsection point to the start of the loop (fast pointer) and
+## the distance from the beginning to the start of the cycle(slow pointer) should be the same.
 
             
